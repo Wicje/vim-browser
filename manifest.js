570cbb39e056d2247fafa2d2620ecc83b31f2f4c
Wicje <anichisom4top@gmail.com>
2025-04-06 19:40:44 +0100

    first commit

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+    "manifest_version": 3,
+    "name": "Vim emulator",
+    "version": "1.0.0",
+    "description": "A basic extension that helps to my browser to operate like vim ",
+    "icons": {
+        "48": images/icon-48.png   //Will look for my own png later
+    }
+}
